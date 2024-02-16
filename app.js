62e71e1c6443490ff0106be61e00d867881aaacb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-16 16:04:57 +0430

    init

--- app.js
+++ app.js
@@ -27,3 +27,5 @@ const deadling = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadling-format h4')
 
 let futureDate = new Date(2024, 4, 24, 11, 30, 0)
+
+const year = futureDate.getFullYear()
