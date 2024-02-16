0578f2c41c987d89a56debf8851c140f4fd31b91
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-16 16:09:32 +0430

    init

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ const items = document.querySelectorAll('.deadling-format h4')
 let futureDate = new Date(2024, 4, 24, 11, 30, 0)
 
 const year = futureDate.getFullYear()
-
+const hours = futureDate.getHours()
 giveaway.textContent = `giveaway ends on ${year}`
