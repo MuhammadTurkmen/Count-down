9a8c2366e9cba7e1089310053e4b84077603f349
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-16 16:02:28 +0430

    init

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ const giveaway = document.querySelector('.giveaway')
 const deadling = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadling-format h4')
 
-let futureDate = new Date()
+let futureDate = new Date(2024, 4, 24, 11, 30, 0)
