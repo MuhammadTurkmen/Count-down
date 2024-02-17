e10e824993e7324f94af97e313d79a12663bcc0d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-17 10:22:57 +0430

    init

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
 
-const giveaway = document.querySelector('.giveaway')
-const deadling = document.querySelector('.deadline')
-const items = document.querySelectorAll('.deadling-format h4')
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+// months are ZERO index based;
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
-let futureDate = new Date(2024, 4, 24, 11, 30, 0)
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
 
-const year = futureDate.getFullYear()
-const hours = futureDate.getHours()
-const minutes = futureDate.getMinutes()
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
+let month = futureDate.getMonth();
+month = months[month];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 
-let month = futureDate.getMonth()
-month = months[month]  
-const date = futureDate.getDate()
+const futureTime = futureDate.getTime();
+function getRemaindingTime() {
+  const today = new Date().getTime();
 
-
-const weekday = weekdays[futureDate.getDay()]
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`
-
-
-// future time in ms  
-const futureTime = futureDate.getTime()
-
-function getRemainingTime() {
-  const today = new Date().getTime
-  const t = futureTime - today
+  const t = futureTime - today;
   // 1s = 1000ms
   // 1m = 60s
-  // 1hr = 60min
+  // 1hr = 60m
   // 1d = 24hr
+  // values in miliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
 
-  // values in ms
-  const oneDay = 24 * 60 * 60 * 1000
-  const oneHour = 60 * 60 * 1000
-  const oneMinute = 60 * 1000
-  // calculate all value
-  let days = t/oneDay
-  days = Math.floor(days)
-  let hours = Math.floor((t % oneDay) / oneHour)
-  let minutes = Math.floor((t % oneHour) / oneMinute)
-  let seconds = Math.floor((t % oneMinute) / 1000)
+  // set values array
+  const values = [days, hours, minutes, seconds];
 
-  // set values array 
-  const values = [days, hours, minutes, seconds]
+  
 
-  items.forEach(function(item, index) {
-    item.innerHTML = values[index]
-  })
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+
 
 }
-
-getRemainingTime()
+//set initial values
+getRemaindingTime();
