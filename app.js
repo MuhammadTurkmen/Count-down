032948bcf8f39380692564f42d7a0bc41ae6c624
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-17 10:40:21 +0430

    init

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-
+let tempDate = new Date()
 
 let  futureDate = new Date(2024, 3, 24, 11, 0, 0)
 
