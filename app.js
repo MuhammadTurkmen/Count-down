346a85c7f23d5dc510f7235347178efa07609331
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-17 10:39:58 +0430

    init

--- app.js
+++ app.js
@@ -25,14 +25,9 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let tempDate = new Date();
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth();
-let tempDay = tempDate.getDate();
-// months are ZERO index based;
-const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
-// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+
+let  futureDate = new Date(2024, 3, 24, 11, 0, 0)
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
